9eae52bcdfb31cb103d4356439bb78aaaa5b1cdd
flavioxx <35305267+flavioxx@users.noreply.github.com>
2024-05-29 17:44:58 -0300

    Update AppFooter.vue

--- src/layouts/default/AppFooter.vue
+++ src/layouts/default/AppFooter.vue
@@ -1,47 +1,5 @@
 <template>
-  <v-footer absolute app class="mt-10">
-    <div class="d-flex flex-grow-1 flex-wrap gap-y-1 align-end">
-      <div class="flex-shrink-0 d-flex gap-2 align-center">
-        <v-btn
-        @click="about"
-        variant="tonal"
-          size="small"
-          color="blue"
-          >
-          <v-icon start> mdi-information </v-icon>
-          {{ $t("about.title") }}
-        </v-btn>
-        <v-btn
-        @click="contribute"
-          variant="tonal"
-          size="small"
-          color="light-blue-lighten-1"
-        >
-          <v-icon start>mdi-hand-coin</v-icon>
-          {{ $t("contribute.button") }}
-        </v-btn>
-        <p style="font-size: 12px" class="text-disabled">v{{ version }}</p>
-      </div>
-      <v-spacer />
-      <div class="d-flex gap-x-1">
-        <v-btn
-          v-for="(link, i) in links"
-          size="small"
-          variant="tonal"
-          :key="i"
-          :href="link.url"
-          target="_blank"
-        >
-          <v-icon v-if="link.icon" :start="!!link.title">
-            {{ link.icon }}
-          </v-icon>
-          {{ link.title }}
-        </v-btn>
-      </div>
-    </div>
-  </v-footer>
-  <Contribute ref="contribute" />
-  <About ref="about" />
+
 </template>
 
 <script>
