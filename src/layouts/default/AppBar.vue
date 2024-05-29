c1b8cfbf552097bb6ac4712eb96182521ac6a6bc
flavioxx <35305267+flavioxx@users.noreply.github.com>
2024-05-29 17:52:19 -0300

    Update AppBar.vue

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -2,8 +2,6 @@
   <v-app-bar flat>
     <v-app-bar-title class="flex-shrink-0">
       <v-btn variant="text" @click="$router.push({ name: 'instances' })">
-        <v-img src="@/assets/logo.png" height="24" width="24" class="mr-2" />
-        Evolution Manager
       </v-btn>
     </v-app-bar-title>
     <v-icon v-if="AppStore.connecting" color="info">
