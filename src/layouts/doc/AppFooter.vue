5125a4e9e18023018d622bfe520fe9fab103bdef
flavioxx <35305267+flavioxx@users.noreply.github.com>
2024-05-29 17:40:46 -0300

    Update AppFooter.vue

--- src/layouts/doc/AppFooter.vue
+++ src/layouts/doc/AppFooter.vue
@@ -1,5 +1,32 @@
-
-
+<template>
+  <v-footer absolute app class="mt-10">
+    <div class="d-flex flex-grow-1 flex-wrap gap-y-1 align-end">
+      <div class="flex-shrink-0 d-flex gap-2 align-center">
+        <v-btn
+        @click="about"
+        variant="tonal"
+          size="small"
+          color="blue"
+          >
+          <v-icon start> mdi-information </v-icon>
+          Sobre
+        </v-btn>
+        <v-btn
+        @click="contribute"
+          variant="tonal"
+          size="small"
+          color="light-blue-lighten-1"
+        >
+          <v-icon start>mdi-hand-coin</v-icon>
+          Contribua com o projeto
+        </v-btn>
+        <p style="font-size: 12px" class="text-disabled">v{{ version }}</p>
+      </div>
+    </div>
+  </v-footer>
+  <Contribute ref="contribute" />
+  <About ref="about" />
+</template>
 <script>
 import About from "@/components/modal/About.vue";
 import Contribute from "@/components/modal/Contribute.vue";
