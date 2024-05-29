027c1a71bcdbd81512b19d6867aa6722467151fe
flavioxx <35305267+flavioxx@users.noreply.github.com>
2024-05-29 17:46:27 -0300

    Update AppFooter.vue

--- src/layouts/default/AppFooter.vue
+++ src/layouts/default/AppFooter.vue
@@ -3,46 +3,7 @@
 </template>
 
 <script>
-import About from "@/components/modal/About.vue";
-import Contribute from "@/components/modal/Contribute.vue";
-import { version } from "../../../package.json";
 
-export default {
-  name: "AppFooter",
-  data: () => ({
-    version,
-    links: [
-      {
-        title: "Discord",
-        url: "https://discord.gg/U66zvhV49B",
-      },
-      {
-        title: "Postman",
-        url: "https://www.postman.com/agenciadgcode/workspace/evolution-api/overview",
-      },
-      {
-        title: "Evolution-Api",
-        icon: "mdi-github",
-        url: "https://github.com/EvolutionAPI/evolution-api",
-      },
-      {
-        title: "Doc",
-        icon: "mdi-book-open-page-variant",
-        url: "https://doc.evolution-api.com",
-      },
-    ],
-  }),
-  methods: {
-    
-    about() {
-      this.$refs.about.open();
-    },
-    contribute() {
-      this.$refs.contribute.open();
-    },
-  },
-  components: { Contribute, About },
-};
 </script>
 
 <style></style>
