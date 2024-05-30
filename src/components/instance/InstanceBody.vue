16c9dfecb62b799e805de77cc68f9ec0b74e6c24
flavioxx <35305267+flavioxx@users.noreply.github.com>
2024-05-29 22:52:47 -0300

    Update InstanceBody.vue

--- src/components/instance/InstanceBody.vue
+++ src/components/instance/InstanceBody.vue
@@ -67,7 +67,7 @@ export default {
           "Webhook",
           "Websocket",
           "Rabbitmq",
-          "Chatwoot",
+          "Painel de Atendimento",
           "Typebot",
         ],
       },
